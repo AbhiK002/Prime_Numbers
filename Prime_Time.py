686d079fa7dc856a64b59cbfb9323e10a93c947a
ppsilva9029 <ajoseeduardoa@gmail.com>
2022-02-03 19:10:30 -0700

    Function implemented

--- Prime_Time.py
+++ Prime_Time.py
@@ -12,6 +12,10 @@ def is_prime(n):
     if n % 2 == 0 and n != 2:
         return False
 
+    # to not count negative numbers
+    if n < 0:
+        return False
+
     #Yo could improve this function replacing n with sqrt(n)
     for i in range(3, n):
         if n % i == 0:
@@ -37,26 +41,16 @@ while True:
         print("")
         low_limit = int(input("Enter the lower limit: "))
         max_limit = int(input("Enter the upper limit: "))
-        output = ""
+
         if max_limit == 0 and low_limit == 0:
             print("Exiting Program...")
             time.sleep(2)
             break
         if low_limit > max_limit:
             print("Wrong Interval. (Lower Limit > Upper Limit)")
-        for number in range(low_limit + 1,max_limit):
-            if number != 1 and number != 0:
-                div_count = 0
-                for factor in range(number + 1):
-                    if factor != 0:
-                        if number % factor == 0:
-                            div_count += 1
-                if div_count == 2:
-                    output += str(number) + ", "
-        if output != "":
-            print(output[:-2] + '.')
-        elif output == "":
-            print("None")
+            continue
+
+        count_primes_b2n(low_limit, max_limit)
     except ValueError:
         print("Enter a number only.")
 print('\n' + "Thank You!")
